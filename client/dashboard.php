dfe7180a1a2bc960672ace153201477adc9d4576
MdAdamgithub <133992132+MdAdamgithub@users.noreply.github.com>
2024-02-18 17:15:54 +0530

    Update dashboard.php

--- client/dashboard.php
+++ client/dashboard.php
@@ -106,7 +106,7 @@ $(".sidebar-icon").click(function() {
 	
 </center>
 	<div>
-		</iframe><iframe src='https://webchat.botframework.com/embed/LanguageCMS-bot?s=OsxCWo-Eixc.Da5AHXZdRuv34CnedwT6X0nqw0wj-t4LgzPhHU4T-Rk'  style='min-width: 400px; width: 70%; min-height: 500px;'></iframe>
+		</iframe><iframe src='https://webchat.botframework.com/embed/LanguageCMS-bot?s=OsxCWo-Eixc.Da5AHXZdRuv34CnedwT6X0nqw0wj-t4LgzPhHU4T-Rk'  style='min-width: 400px; width: 70%; min-height: 500px; margin-left: 100px;'></iframe>
 	</div>
 	<center>
 
